16141fc3782e2650349f79c2a98a9c8aff7a5730
tko-dev <kogucki@kth.se>
2026-02-12 11:08:49 +0000

    Deploying to gh-pages from @ tko-dev/tko-dev.github.io@aeda0ab34e35dc8341fd17aa9d93c6aa41d2c538 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,11 +42,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
